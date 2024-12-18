71865e3d5660e97d5057590aa212756a2764c6af
Xeimyn <github@jcms.dev>
2024-12-18 08:05:09 +0100

    🚧 Database v0.1

--- src/database/Database.py
+++ src/database/Database.py
@@ -1,52 +1,68 @@
+from sqlalchemy import Engine
 from sqlmodel import SQLModel, create_engine,Session, select
 from src.database.Model import Thought
 
 class Database:
-	def __init__(self):
-		self.engine = create_engine("sqlite:///data//thoughts.db")
-		SQLModel.metadata.create_all(bind=self.engine)
+	def __init__(self) -> None:
+		self.engine:Engine = create_engine("sqlite:///data//thoughts.db")
+		SQLModel.metadata.create_all(self.engine)
 
-	def addThought(self,title,description,html,css=None):
+	def addThought(self,title,description,html,css=None) -> None:
 		newThought = Thought(
 				title=title,
 				description=description,
 				htmlContent=html,
 				cssContent=css)
-		with Session(bind=self.engine) as session:
+		with Session(self.engine) as session:
 			session.add(instance=newThought)
 			session.commit()
 
-	def deleteThought(self,thoughtId):
-		with Session(bind=self.engine) as session:
-			thought = session.exec(select(Thought).where(Thought.thoughtId == thoughtId)).first()
+	def deleteThought(self,thoughtId) -> None:
+		thought:Thought | None = self._getThoughtById(thoughtId)
+		with Session(self.engine) as session:
 			session.delete(thought)
+			session.commit()
 
-	def editThought(self,thoughtId,title=None,description=None,html=None,css=None):
-		toUpdate = [item for item in [title,description,html,css] if item != None]
-		# If there is nothing to update return early
-		if len(toUpdate) == 0:
-			return False
-		with Session(bind=self.engine) as session:
-			thought = session.exec(select(Thought).where(Thought.thoughtId == thoughtId)).first()
-			for item in toUpdate:
-				thought.
+	def editThought(self,thoughtId,title=None,description=None,html=None,css=None) -> None:
+		with Session(self.engine) as session:
+			thought:Thought | None = self._getThoughtById(thoughtId)
+			thought.title = title if title is not None else thought.title
+			thought.description = description if description is not None else thought.description
+			thought.html = html if html is not None else thought.html
+			thought.css = css if css is not None else thought.css
+			# TODO | Not sure if i only have to commit the session after editing the thought
+			session.commit()
 
-	def publishThought(self,thoughtId):
-		print(f"publishing thought with thoughtId {thoughtId}")
-		return "status"
+	def publishThought(self,thoughtId) -> None:
+		with Session(self.engine) as session:
+			thought:Thought | None = self._getThoughtById(thoughtId)
+			thought.public = True
+			session.commit()
 
-	def privateThought(self,thoughtId):
-		print(f"privating thought with thoughtId {thoughtId}")
-		return "status"
+	def privateThought(self,thoughtId) -> None:
+		with Session(self.engine) as session:
+			thought = self._getThoughtById(thoughtId)
+			thought.public = False
+			session.commit()
 
 	def getAllThoughts(self,includeUnpublished=False):
-		print("Getting all thoughts") if includeUnpublished else print("Getting all (public) thoughts")
-		return [{"title":"title","description":"description","createdAt":"createdAt","updatedAt":"updatedAt"},]
+		with Session(self.engine) as session:
+			if includeUnpublished:
+				thoughts = session.exec(select(Thought)).all()
+			else:
+				thoughts = session.exec(select(Thought).where(Thought.public)).all()
+		return thoughts
 
-	def getThoughthoughtIdByTitle(self,title):
-		print("here is your id sir")
-		return "thoughtId"
+	def getThoughtIdByTitle(self,title):
+		with Session(self.engine) as session:
+			thoughtId = session.exec(select(Thought).where(Thought.title == title)).first().thoughtId
+		return thoughtId
 
 	def getThoughtContent(self,thoughtId):
-		print("here is your content sir")
-		return "title,description,html,css,created_at,updated_at"
+		thought = self._getThoughtById(thoughtId)
+		return [thought.title,thought.description,thought.html,thought.css,thought.createdAt,thought.updatedAt]
+
+	def _getThoughtById(self,thoughtId):
+		with Session(self.engine) as session:
+			thought = session.exec(select(Thought).where(Thought.thoughtId == thoughtId)).first()
+		return thought
